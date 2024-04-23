cd91bdd273bdb77ec5f4f8812afb526f18115c61
facuperezm <facundoperezmontalvo@gmail.com>
2024-04-23 13:52:12 -0300

    feat: main auth config done

---
+++ src/auth.ts
@@ -0,0 +1,25 @@
+import GitHub from "next-auth/providers/github";
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import Resend from "next-auth/providers/resend";
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  providers: [
+    Resend({
+      apiKey: process.env.AUTH_RESEND_KEY,
+      from: "no-reply@facupm.dev",
+    }),
+    Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+    GitHub,
+  ],
+});
