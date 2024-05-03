4eaf57f3268d38030688a6995200df18f5f4d0aa
facuperezm <facundoperezmontalvo@gmail.com>
2024-05-03 10:29:59 -0300

    fix

--- src/app/(auth)/auth/login/page.tsx
+++ src/app/(auth)/auth/login/page.tsx
@@ -1,9 +1,12 @@
 import LoginForm from "@/app/(auth)/_components/login-form";
+import { Suspense } from "react";
 
 export default async function LoginPage() {
   return (
     <div className="flex flex-col gap-2 text-black">
-      <LoginForm />
+      <Suspense fallback="Loading...">
+        <LoginForm />
+      </Suspense>
     </div>
   );
 }
