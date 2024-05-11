635354dffb480123bea177d33e16d75b0656b1e8
facuperezm <facundoperezmontalvo@gmail.com>
2024-05-11 13:28:16 -0300

    feat: file uploader

--- src/components/file-uploader.tsx
+++ src/components/file-uploader.tsx
@@ -1,5 +1,39 @@
-import { useDropzone } from "react-dropzone";
+"use client";
+
+import { CloudUpload } from "lucide-react";
+import Dropzone from "react-dropzone";
 
 export default function FileUploader() {
-  return <></>;
+  return (
+    <Dropzone multiple={false} onDrop={async (acceptedFiles) => {}}>
+      {({ getRootProps, getInputProps }) => (
+        <div
+          {...getRootProps()}
+          className="border h-64 m-4 border-dashed border-gray-300 rounded-lg"
+        >
+          <div className="flex items-center justify-center size-full">
+            <label
+              htmlFor="dropzone-file"
+              className="flex flex-col items-center justify-center size-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
+            >
+              <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                <CloudUpload className="h-6 w-6 text-zinc-500 mb-2" />
+                <p className="mb-2 text-sm text-zinc-700">
+                  <span className="font-semibold">Click to upload</span> or drag
+                  and drop
+                </p>
+                <p className="text-xs text-zinc-500">PDF (up to 4MB)</p>
+              </div>
+              <input
+                {...getInputProps()}
+                type="file"
+                id="dropzone-file"
+                className="hidden"
+              />
+            </label>
+          </div>
+        </div>
+      )}
+    </Dropzone>
+  );
 }
